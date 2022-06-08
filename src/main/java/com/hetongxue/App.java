25dbf6ec1af036c326d475bba86cc375f9819b65
hetongxue303 <1974597686@qq.com>
2022-06-08 16:19:06 +0800

    项目初始化

---
+++ src/main/java/com/hetongxue/App.java
@@ -0,0 +1,19 @@
+package com.hetongxue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 入口类
+ * @ClassNmae: App
+ * @Author: 何同学
+ * @DateTime: 2022-06-08 16:15
+ **/
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
