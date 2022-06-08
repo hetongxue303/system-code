5990a536e10c25ad302839fb10920bc0160c025a
hetongxue303 <1974597686@qq.com>
2022-06-08 16:49:32 +0800

    配置数据库

--- system/src/main/java/com/hetongxue/SystemApplication.java
+++ system/src/main/java/com/hetongxue/SystemApplication.java
@@ -1,7 +1,9 @@
 package com.hetongxue;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @Description: 程序入口类
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @DateTime: 2022-06-08 16:38
  */
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.hetongxue.system.mapper")
 public class SystemApplication {
 
     public static void main(String[] args) {
